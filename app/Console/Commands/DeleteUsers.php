6744de344abd2ef32b183e8861be9766bfac5ed6
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-09-05 22:46:35 +0530

    Contact Section WIP

--- app/Console/Commands/DeleteUsers.php
+++ app/Console/Commands/DeleteUsers.php
@@ -5,21 +5,21 @@ namespace App\Console\Commands;
 use App\User;
 use Illuminate\Console\Command;
 
-class TestCommand extends Command
+class DeleteUsers extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'user:create';
+    protected $signature = 'user:delete';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Deletes User Created using faker.';
 
     /**
      * Create a new command instance.
@@ -34,11 +34,19 @@ class TestCommand extends Command
     /**
      * Execute the console command.
      *
+     * @return int
      */
     public function handle()
     {
-        echo "Running Test Command.....\n";
-        echo "Creating 5 users.....\n";
-        factory(User::class, 5)->create();
+        echo "Deleting All Users...\n";
+
+        if (User::query()->count() == 0) {
+            echo "No Users available...\n";
+            return 1;
+        }
+
+        User::query()->delete();
+        echo "Cleaned..\n";
+        return 1;
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteUsers;
 use App\Console\Commands\TestCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         TestCommand::class,
+        DeleteUsers::class
     ];
 
     /**
@@ -25,8 +27,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('ray:test')->everyMinute();
-        $schedule->command('inspire')->everyMinute();
+        $schedule->command('user:create')->everyMinute()->withoutOverlapping;
+        $schedule->command('user:delete')->everyTenMinutes()->withoutOverlapping;
     }
 
     /**
