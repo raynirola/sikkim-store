e7e5004bb670668edabc5afe7f6e7219bd9589de
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-09-05 22:31:39 +0530

    Contact Section WIP

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ray:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        echo "Running Test Command.....\n";
+        echo "Creating 5 users.....\n";
+        factory(User::class, 5)->create();
+    }
+}
