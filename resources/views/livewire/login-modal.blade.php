703f9ca1d33f9cc9da1674619c34ea652c48b0ab
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-09-15 21:19:13 +0530

    Refactoring WIP

--- resources/views/livewire/login-modal.blade.php
+++ resources/views/livewire/login-modal.blade.php
@@ -8,101 +8,103 @@
         </svg>
     </button>
     <div x-show="modal"
-         class="fixed top-0 inset-0 z-30 bg-gray-50 w-screen h-screen bg-gradient-overlay flex items-center justify-center">
-        <div class="w-2/5 bg-gray-50 p-10 rounded-md shadow-md relative font-bodyFont" x-on:click.away="modal = false">
-            <div class="">
-                <a href="{{ route('home.index') }}" class="mb-4">
-                    <img src="{{ asset('images/logo-flat.png') }}"
-                         class="mx-auto h-20 w-20 object-cover rounded-full shadow" alt="">
-                </a>
-                <h2 class="font-titleFont text-center text-lg md:text-xl tracking-wide leading-9 font-normal text-gray-900">
-                    Sign in to your account
-                </h2>
-                <p class="mt-1 text-center text-xs md:text-sm leading-5 text-gray-600">
-                    Or
-                    <a href="{{ '/register' }}"
-                       class="inline-flex items-center font-medium text-green-600 hover:text-green-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                        Register for free
-                        <svg class="h-4 w-4 md:w-5 md:h-5"
-                             xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 20 20"
-                             fill="currentColor">
-                            <path fill-rule="evenodd"
-                                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                  clip-rule="evenodd"/>
-                        </svg>
-                    </a>
-                </p>
-            </div>
-            <form class="mt-8"
-                  wire:submit.prevent="submit">
-                @csrf
+         class="px-4 pb-4 md:pb-0 md:px-0 fixed top-0 inset-0 z-30 w-screen h-screen bg-gradient-overlay">
+        <div class="h-full w-full flex items-end md:items-center justify-center">
+            <div class="w-full md:w-3/5 lg:w-5/12 bg-gray-50 p-10 rounded-md shadow-md relative font-bodyFont" x-on:click.away="modal = false">
                 <div class="">
-                    <div>
-                        <input aria-label="Email address"
-                               wire:model.lazy="email"
-                               name="email"
-                               type="email"
-                               value="{{ old('email') }}"
-                               required
-                               class="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 @error('email'){{'border-red-500'}}@enderror placeholder-gray-500 text-gray-900  focus:outline-none focus:shadow-outline-blue focus:border-green-500 focus:z-10 text-sm sm:leading-5"
-                               placeholder="Email address"/>
-                        @error('email')
-                        <span class="pl-2 text-xs text-red-600 font-light"><strong>{{$message}}</strong></span>
-                        @enderror
-                    </div>
-                    <div class="mt-4">
-                        <input aria-label="Password"
-                               wire:model.lazy="password"
-                               name="password"
-                               type="password"
-                               required
-                               class="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 @error('password'){{'border-red-500'}}@enderror placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-green-500 focus:z-10 text-sm sm:leading-5 relative"
-                               placeholder="Password"/>
-                        @error('password')
-                        <span class="pl-2 text-xs text-red-600 font-light"><strong>{{$message}}</strong></span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="mt-6 flex items-center justify-between">
-                    <div class="flex items-center text-sm text-gray-600">
-                        <input id="remember"
-                               type="checkbox"
-                               wire:model.laxy="remember"
-                               name="remember"
-                               class="form-checkbox h-4 w-4 text-brand-700 transition duration-150 ease-in-out mr-2"
-                        {{old('remember') == 'on' ? 'checked' : ''}}
-                        <label for="remember"
-                               class="block leading-5">
-                            Remember me
-                        </label>
-                    </div>
-
-                    <div class="text-sm leading-5">
-                        <a href="{{ '/password/reset/' }}"
-                           class="font-normal text-brand-700 underline hover:text-brand-600 focus:outline-none focus:underline transition ease-in-out duration-150">
-                            Forgot your password?
+                    <a href="{{ route('home.index') }}" class="mb-4">
+                        <img src="{{ asset('images/logo-flat.png') }}"
+                             class="mx-auto h-20 w-20 object-cover rounded-full shadow" alt="">
+                    </a>
+                    <h2 class="font-titleFont text-center text-lg md:text-xl tracking-wide leading-9 font-normal text-gray-900">
+                        Sign in to your account
+                    </h2>
+                    <p class="mt-1 text-center text-xs md:text-sm leading-5 text-gray-600">
+                        Or
+                        <a href="{{ '/register' }}"
+                           class="inline-flex items-center font-medium text-green-600 hover:text-green-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                            Register for free
+                            <svg class="h-4 w-4 md:w-5 md:h-5"
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 20 20"
+                                 fill="currentColor">
+                                <path fill-rule="evenodd"
+                                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                      clip-rule="evenodd"/>
+                            </svg>
                         </a>
+                    </p>
+                </div>
+                <form class="mt-8"
+                      wire:submit.prevent="submit">
+                    @csrf
+                    <div class="">
+                        <div>
+                            <input aria-label="Email address"
+                                   wire:model.lazy="email"
+                                   name="email"
+                                   type="email"
+                                   value="{{ old('email') }}"
+                                   required
+                                   class="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 @error('email'){{'border-red-500'}}@enderror placeholder-gray-500 text-gray-900  focus:outline-none focus:shadow-outline-blue focus:border-green-500 focus:z-10 text-sm sm:leading-5"
+                                   placeholder="Email address"/>
+                            @error('email')
+                            <span class="pl-2 text-xs text-red-600 font-light"><strong>{{$message}}</strong></span>
+                            @enderror
+                        </div>
+                        <div class="mt-4">
+                            <input aria-label="Password"
+                                   wire:model.lazy="password"
+                                   name="password"
+                                   type="password"
+                                   required
+                                   class="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 @error('password'){{'border-red-500'}}@enderror placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-green-500 focus:z-10 text-sm sm:leading-5 relative"
+                                   placeholder="Password"/>
+                            @error('password')
+                            <span class="pl-2 text-xs text-red-600 font-light"><strong>{{$message}}</strong></span>
+                            @enderror
+                        </div>
                     </div>
-                </div>
 
-                <div class="mt-6">
-                    <button type="submit"
-                            class="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-brand-700 hover:bg-brand-600 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-700 transition duration-150 ease-in-out">
-                        Sign in
-                    </button>
-                </div>
-            </form>
+                    <div class="mt-6 flex items-center justify-between">
+                        <div class="flex items-center text-sm text-gray-600">
+                            <input id="remember"
+                                   type="checkbox"
+                                   wire:model.laxy="remember"
+                                   name="remember"
+                                   class="form-checkbox h-4 w-4 text-brand-700 transition duration-150 ease-in-out mr-2"
+                            {{old('remember') == 'on' ? 'checked' : ''}}
+                            <label for="remember"
+                                   class="block leading-5">
+                                Remember me
+                            </label>
+                        </div>
 
-            <div class="absolute right-0 top-0 text-black p-2 flex items-center justify-center cursor-pointer"
-                 x-on:click="modal = false">
-                <svg class="w-4 h-4 text-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                     fill="currentColor">
-                    <path fill-rule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clip-rule="evenodd"/>
-                </svg>
+                        <div class="text-sm leading-5">
+                            <a href="{{ '/password/reset/' }}"
+                               class="font-normal text-brand-700 underline hover:text-brand-600 focus:outline-none focus:underline transition ease-in-out duration-150">
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="mt-6">
+                        <button type="submit"
+                                class="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-brand-700 hover:bg-brand-600 focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-700 transition duration-150 ease-in-out">
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+
+                <div class="absolute right-0 top-0 text-black p-2 flex items-center justify-center cursor-pointer"
+                     x-on:click="modal = false">
+                    <svg class="w-4 h-4 text-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                         fill="currentColor">
+                        <path fill-rule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </div>
             </div>
         </div>
     </div>
