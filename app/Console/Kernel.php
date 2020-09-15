50d9f4a60b0f7c42922e0b146860692b371f297a
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-09-15 10:52:22 +0530

    WIP

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:dailyMail')->dailyAt('10:50, 10:55');
+        $schedule->command('send:dailyMail')->dailyAt('10:55');
     }
 
     /**
