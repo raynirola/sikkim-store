385feb719378398cbf85afdafa15c28fc1a06a00
Ray Nirola <54945029+raynirola@users.noreply.github.com>
2020-11-06 22:14:07 +0530

    NEW: Landing Page Redesigned.

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -72,7 +72,7 @@
                         </p>
                         <p class="mt-5 sm:mt-12 mb-2 text-gray-400 text-sm">Sign up for early access when we launch.</p>
                         <div class="sm:flex sm:justify-center lg:justify-start">
-                            <div class="rounded-md shadow" x-data="{false, toast : true}">
+                            <div class="rounded-md shadow" x-data="{modal : false, toast : true}">
                                 <a href="#" x-on:click="modal = true"
                                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
                                     Register Now
